27a177d7fb12feca6efc554554ca237c9f4b1f10
wuhuanhost <post.thinking@gmail.com>
2016-12-29 18:56:39 +0800

    添加了大文件续传的文件，功能还在实现中

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,10 @@ router.get('/', function (req, res, next) {
  */
 router.post('/upload', function (req, res, next) {
     var form = new multiparty.Form();
-    // form.on('error', function(err) {
-    //     console.log('Error parsing form: ' + err.stack);
-    // });
+
+    form.on('error', function (err) {
+        console.log('Error parsing form: ' + err.stack);
+    });
     form.encoding = 'utf-8';
     form.uploadDir = "public/uploads/images/";
     form.maxFilesSize = 2 * 1024 * 1024;
@@ -34,6 +35,57 @@ router.post('/upload', function (req, res, next) {
             "alt": fields.fileRemark[0]
         })
     });
+
 });
+
+
+
+
+router.post("/load", function (req, res, next) {
+    var form = new multiparty.Form();
+    var count;
+    form.encoding = 'utf-8';
+    form.uploadDir = "public/uploads/images/";
+    form.maxFilesSize = 2 * 1024 * 1024;
+    form.on('error', function (err) {
+        console.log('Error parsing form: ' + err.stack);
+    });
+
+    // Parts are emitted when parsing the form 
+    form.on('part', function (part) {
+        // You *must* act on the part by reading it 
+        // NOTE: if you want to ignore it, just call "part.resume()" 
+        if (!part.filename) {
+            // filename is not defined when this is a field and not a file 
+            console.log('got field named ' + part.name);
+            // ignore field's content 
+            part.resume();
+        }
+        
+        if (part.filename) {
+            // filename is defined when this is a file 
+            count++;
+            console.log('got file named ' + part.name);
+            // ignore file's content here 
+            part.resume();
+        }
+
+        part.on('error', function (err) {
+            // decide what to do 
+        });
+    });
+
+    // Close emitted after form parsed 
+    form.on('close', function () {
+        console.log('Upload completed!');
+    });
+
+    // Parse req 
+    form.parse(req);
+
+
+})
+
+
 
 module.exports = router;
